8839acf2e6526cfc80dc391851b65fcb88c37102
Ryxian <Ryxians@gmail.com>
2025-06-14 15:27:07 -0500

    Initial commit

---
+++ test/ec2-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ec2Template from '../lib/ec2-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ec2-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ec2Template.Ec2TemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
